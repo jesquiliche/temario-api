47f5f34d59f64d3d02a77227b47206216e87c35c
Jesus Quintana <jesquiliche@hotmail.com>
2022-01-23 01:38:40 +0100

    API V2.1

--- src/mongoose.js
+++ src/mongoose.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 
 
-//const uri = `mongodb://localhost/temario`;
-console.log(process.env.USER)
-const uri = `mongodb+srv://jesquiliche:3912481B@cluster0.m14ql.mongodb.net/temario?authSource=admin&replicaSet=atlas-jjcdpd-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`;
+//const uri = `mongodb://localhost/temario`;}
+const user = process.env.USER;
+const password=process.env.PASSWORD;
 
+const uri = `mongodb+srv://${user}:${password}@cluster0.m14ql.mongodb.net/temario?authSource=admin&replicaSet=atlas-jjcdpd-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`;
+console.log("entro")
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(()=> console.log('conectado a mongodb')) 
   .catch(e => console.log('error de conexión', e))
